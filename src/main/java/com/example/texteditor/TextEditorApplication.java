113c46430165fb7b8979ae4fa56481c26f3d14d9
Gayle Hoefer <gaylehoefer@gmail.com>
2023-01-17 19:58:15 -0700

    Add try block to save file name from user input

--- src/main/java/com/example/texteditor/TextEditorApplication.java
+++ src/main/java/com/example/texteditor/TextEditorApplication.java
@@ -1,13 +1,48 @@
 package com.example.texteditor;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
+import java.util.*;
 
-@SpringBootApplication
+
 public class TextEditorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TextEditorApplication.class, args);
-    }
 
+        Scanner scanner = new Scanner(System.in);
+        Scanner fileIn; //input file connection
+        PrintWriter fileout; //HTML file connection
+        String filenameIn; // original file's name
+        String filenameOut;// new HTML file's name
+        int dotIndex; //postion of . in filename
+        String line = null; // a line from the imput file
+
+        System.out.println("Enter file name or path");
+        filenameIn = scanner.nextLine();
+        try {
+            fileIn = new Scanner(new FileReader(filenameIn));
+            dotIndex = filenameIn.lastIndexOf(".");
+            if (dotIndex == -1) {
+                filenameOut = filenameIn + ".html";
+            }
+            else {
+                filenameOut = filenameIn.substring(0,dotIndex) + ".html";
+            }
+            fileout = new PrintWriter(filenameOut);
+
+            try {
+                line = fileIn.nextLine();
+            }
+            catch(NoSuchElementException e) {
+                System.out.println("Error: "+e.getMessage());
+            }
+            if(line==null) {
+                System.out.println("This file is empty : (");
+            }
+            else {
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
